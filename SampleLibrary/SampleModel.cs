801152dc3388fa6b8e53f2fd6849ee8f8ed3aa6c
ezbishal <ezbishal@gmail.com>
2024-03-13 22:34:21 +0000

    Added sample models for testing

---
+++ SampleLibrary/SampleModel.cs
@@ -0,0 +1,11 @@
+﻿using DtoGenerator.Attributes;
+
+namespace SampleLibrary;
+
+[GenerateDto("CreateSampleModel", "GetSampleModel")]
+public class SampleModel
+{
+    public string PropToInclude { get; set; }
+    [ExcludeProperty("CreateSampleModel")]
+    public string PropToExclude { get; set; }
+}
